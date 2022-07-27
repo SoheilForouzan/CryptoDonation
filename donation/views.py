0e53a020a358effaea23ec9c4b6a572a2efb75ec
Soheil Forouzan <soheil.forouzan@protonmail.com>
2022-07-28 02:20:08 +0430

    Updated requirements and added final transaction

--- donation/views.py
+++ donation/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from bitcoin import *
+from bitcoinlib.wallets import Wallet
 import qrcode
 from PIL import Image
 from PIL import ImageDraw
@@ -7,24 +7,31 @@ from eth_account import Account
 import secrets
 
 def BTCDonation(request):
-    # generate a btc wallet address for the user
-    private_key = random_key()
-    public_key = privtopub(private_key)
-    address = pubtoaddr(public_key)
+    # generate a btc wallet with bitcoinlib
+    wallet = Wallet.create(f'{secrets.token_hex(32)}')
+    key1 = wallet.get_key()
+    address = key1.address    
+    
 
-    # generate a payment request with qrcode
+    # Generate a payment request with qrcode
     payment_request = "bitcoin:" + address + "?amount=0.01"
     qrmake = qrcode.make(payment_request)
     qrmake.save('donation/static/donation/btcs.png')
-    qrpath = f'static/donation/btcs.png'  
+    qrpath = f'static/donation/btcs.png'
 
-    
+    # Generate a transaction
+
+    # destination = "bc1qklqkjy8d79gdnkeagwtykeh2ejdadrar3pr46x"
+    # tx = wallet.send_to(destination, 0.01)
+
     context = {
-        'qrcode': qrpath,
         'address': address,
+        'qrcode': qrpath,
     }
 
     return render(request, 'donation/test.html', context)
+
+    
 
 def ETHDonation(request):
     # Generate a eth wallet address for the user
@@ -38,6 +45,22 @@ def ETHDonation(request):
     qrmake = qrcode.make(payment_request)
     qrmake.save('donation/static/donation/eths.png')
     qrpath = f'static/donation/eths.png'
+
+
+    # Generate a transaction
+
+    # destination = "0x00bf6C66241897CB5E3d3819f3B87240453e86D3"
+    # tx = Transaction(
+    #     nonce=0,
+    #     gas_price=10000000000,
+    #     gas=21000,
+    #     to=destination,
+    #     value=10000000000,
+    #     data=b'',
+    #     chain_id=1
+    # )
+    # tx.sign(private_key)
+    # tx_signed = tx.raw
 
     context = {
         'qrcode': qrpath,
