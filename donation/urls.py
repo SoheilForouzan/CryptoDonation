d92ad4102124a6814ed257c6292d0b0f8e5928cd
Soheil Forouzan <soheil.forouzan@protonmail.com>
2022-07-28 21:30:38 +0430

    USDT Added

--- donation/urls.py
+++ donation/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('btc', views.BTCDonation, name='BTCDonation'),
     path('eth', views.ETHDonation, name='ETHDonation'),
-    
+    path('usdt', views.USDTDonation, name='USDTDonation'),
 ]
--- donation/views.py
+++ donation/views.py
@@ -6,12 +6,14 @@ from PIL import ImageDraw
 from eth_account import Account
 import secrets
 
+# BTC
+
+
 def BTCDonation(request):
     # generate a btc wallet with bitcoinlib
     wallet = Wallet.create(f'{secrets.token_hex(32)}')
     key1 = wallet.get_key()
-    address = key1.address    
-    
+    address = key1.address
 
     # Generate a payment request with qrcode
     payment_request = "bitcoin:" + address + "?amount=0.01"
@@ -31,21 +33,56 @@ def BTCDonation(request):
 
     return render(request, 'donation/test.html', context)
 
-    
 
+# ETH
 def ETHDonation(request):
     # Generate a eth wallet address for the user
     priv = secrets.token_hex(32)
     private_key = "0x" + priv
     acct = Account.from_key(private_key)
     address = acct.address
-    
+
     # Generate a payment request with qrcode
     payment_request = "ethereum:" + address + "?amount=0.01"
     qrmake = qrcode.make(payment_request)
     qrmake.save('donation/static/donation/eths.png')
     qrpath = f'static/donation/eths.png'
 
+    # Generate a transaction
+
+    # destination = "0x00bf6C66241897CB5E3d3819f3B87240453e86D3"
+    # tx = Transaction(
+    #     nonce=0,
+    #     gas_price=10000000000,
+    #     gas=21000,
+    #     to=destination,
+    #     value=10000000000,
+    #     data=b'',
+    #     chain_id=1
+    # )
+    # tx.sign(private_key)
+    # tx_signed = tx.raw
+
+    context = {
+        'qrcode': qrpath,
+        'address': address,
+    }
+
+    return render(request, 'donation/test.html', context)
+
+
+# USDT
+def USDTDonation(request):
+    # Generate a usdt wallet address for the user
+    wallet = Wallet.create(f'{secrets.token_hex(32)}')
+    key1 = wallet.get_key()
+    address = key1.address
+
+    # Generate a payment request with qrcode
+    payment_request = "ethereum:" + address + "?amount=0.01"
+    qrmake = qrcode.make(payment_request)
+    qrmake.save('donation/static/donation/usdts.png')
+    qrpath = f'static/donation/usdts.png'
 
     # Generate a transaction
 
